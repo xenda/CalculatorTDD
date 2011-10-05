bc0f86a2223a9f0eb89c0c6be6e6417ee24808ff
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2011-10-04 22:18:22 -0500

    refactors on code and specs

--- Calculadora.rb
+++ Calculadora.rb
@@ -1,24 +1,24 @@
 #Calculadora.rb
 class Calculadora
 
-	def suma(a , b)
-		a + b
-	end
+  def suma(a , b)
+    a + b
+  end
 
-	def resta(c, d)
-		c - d
-	end
+  def resta(c, d)
+    c - d
+  end
 
-	def multiplica(e, f)
-		e * f
-	end
+  def multiplica(e, f)
+    e * f
+  end
 
-	def no_es_cero(g)
-		g != 0
-	end
+  def es_cero?(g)
+    g == 0
+  end
 
-	def divide (numerador, denominador)
-		no_es_cero(denominador) ? numerador / denominador : 0		
-	end
-	
+  def divide (numerador, denominador)
+    ( numerador / denominador ) unless es_cero?(denominador)
+  end
+
 end
